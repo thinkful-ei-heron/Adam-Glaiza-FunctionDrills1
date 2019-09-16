158d3ac39f9567759e6d98780d87368a33970d57
Glaiza Wagner <glaizawagner@gmail.com>
2019-09-16 14:22:54 -0700

    FinalDrill1

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,27 +1,24 @@
-'use strict';
-function yearOfBirth(age) {
-    if (age < 0) {
-        throw new Error("Age can not be negative");
-    }
-    return 2019 - age;
-}
+'use strict'; 
+
+function yearOfBirth(age) { return 2019 - age; }
 
 function createGreeting(name, age) {
-    const yob= yearOfBirth();
-    if(name !== null && parseInt(age) !== null) {
-        return `Hi my name is ${name} and I'm ${age} years old.`;
-        
-    } else if (typeof name !== "string" || typeof age !== "number") {
-        throw new Error("Something wrong with the name/age");
-    }
-    else {
-        console.log('Arguments not valid');
-    }
-    
+
+if(name === undefined || age === undefined) {
+    throw new Error('Arguments not valid');
+} 
+else if (typeof name !== "string" || typeof age !== "number") {
+    throw new Error('Something wrong with the name/age');
+}    
+else if (age < 0) {
+    return new Error('Age cannot be negative')
+}
+const yob= yearOfBirth(age);
+return `Hi my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
 }
 
-try {
-const greeting1 = createGreeting('Adam', 32);
-} catch (error) {
-    console.log(error,error.message);
+try { const greeting1 = createGreeting(4, 32); 
+    console.log(greeting1); 
+} catch (error) { 
+    console.log(error); 
 }
