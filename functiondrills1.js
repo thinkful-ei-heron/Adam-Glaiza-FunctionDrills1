659809e1999a54c1e877cf9429cfca2b26dbdce2
Adam Newhouser <adamnewhouser@gmail.com>
2019-09-16 16:11:02 -0400

    #6

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,7 +1,10 @@
 'use strict';
+function yearOfBirth(age) {
+    return 2019 - age;
+}
 function createGreeting(name, age) {
-    const yearOfBirth = 2019 - age;
-    return `Hi my name is add ${name} and I'm ${age} years old.`;
+    const yob= yearOfBirth();
+    return `Hi my name is ${name} and I'm ${age} years old.`;
 }
 const greeting1 = createGreeting('Adam', 32);
 createGreeting(greeting1);
