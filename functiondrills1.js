a3ff98810baff71b1a422dbceb73524b09e4aab6
Adam Newhouser <adamnewhouser@gmail.com>
2019-09-16 15:52:17 -0400

    Drills1

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,6 +1,6 @@
 'use strict';
 function createGreeting(name, age) {
-    console.log(`Hi my name is ${name} and I'm ${age} years old.`);
+    return `Hi my name is ${name} and I'm ${age} years old.`;
 }
-
-createGreeting('Glaiza', 35);
+const greeting1 = createGreeting('Adam', 32);
+createGreeting(greeting1);
