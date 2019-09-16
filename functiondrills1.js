156223e209f56b1d937371c8a6b90037a308155c
Glaiza Wagner <glaizawagner@gmail.com>
2019-09-16 13:31:23 -0700

    #7

--- functiondrills1.js
+++ functiondrills1.js
@@ -1,10 +1,27 @@
 'use strict';
 function yearOfBirth(age) {
+    if (age < 0) {
+        throw new Error("Age can not be negative");
+    }
     return 2019 - age;
 }
+
 function createGreeting(name, age) {
     const yob= yearOfBirth();
-    return `Hi my name is ${name} and I'm ${age} years old.`;
+    if(name !== null && parseInt(age) !== null) {
+        return `Hi my name is ${name} and I'm ${age} years old.`;
+        
+    } else if (typeof name !== "string" || typeof age !== "number") {
+        throw new Error("Something wrong with the name/age");
+    }
+    else {
+        console.log('Arguments not valid');
+    }
+    
 }
+
+try {
 const greeting1 = createGreeting('Adam', 32);
-createGreeting(greeting1);
+} catch (error) {
+    console.log(error,error.message);
+}
